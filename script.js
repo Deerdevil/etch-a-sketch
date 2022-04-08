8f6dfbb0857638a0df61c9b4f1096970ce14e88e
Deerdevil <nicholai.oj@gmail.com>
2022-04-08 15:38:16 +0200

    Fixed resetbutton, added random colors.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict:";
 const canvas = document.querySelector(".canvas");
-const mainCanvas = document.getElementById("main--canvas");
+
+const mainCanvas = document.getElementById("start--canvas");
 const canvasButtons = document.querySelector(".canvas--buttons");
 const btnCanvas16 = document.querySelector(".btn--create--16");
 const btnCanvas32 = document.querySelector(".btn--create--32");
@@ -9,6 +10,7 @@ const btnReset = document.querySelector(".btn--reset");
 
 //Create canvas
 function createCanvas16() {
+  mainCanvas.id = "main--canvas";
   mainCanvas.classList.remove("canvas");
   mainCanvas.classList.add("canvas--16");
   for (let i = 0; i < 256; i++) {
@@ -17,11 +19,13 @@ function createCanvas16() {
 
     canvas16.appendChild(createDiv);
   }
+
   addColorClass();
   draw();
 }
 
 function createCanvas32() {
+  mainCanvas.id = "main--canvas";
   mainCanvas.classList.remove("canvas");
   mainCanvas.classList.add("canvas--32");
   for (let i = 0; i < 1024; i++) {
@@ -33,6 +37,7 @@ function createCanvas32() {
   draw();
 }
 function createCanvas64() {
+  mainCanvas.id = "main--canvas";
   mainCanvas.classList.remove("canvas");
   mainCanvas.classList.add("canvas--64");
   for (let i = 0; i < 4096; i++) {
@@ -45,21 +50,40 @@ function createCanvas64() {
 }
 function resetCanvas() {
   mainCanvas.replaceChildren();
+  mainCanvas.removeAttribute("class");
   mainCanvas.classList.add("canvas");
+  document.querySelector(".canvas").style.backgroundColor = "";
+  mainCanvas.style.backgroundColor = "";
+  mainCanvas.id = "start--canvas";
 }
 //DRAW on the canvas
+let randomColorArr = ["red", "green", "blue", "pink"];
+function randomColor(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  return item;
+}
+
 function draw() {
+  const selectedColor = randomColor(randomColorArr);
+  mainCanvas.id = "main--canvas";
   const colors = document.getElementById("main--canvas");
   //Checks if target is within the main canvas and applies color
+
   colors.onmouseover = function (event) {
-    let target = event.target;
-    target.style.backgroundColor = "blue";
+    if (colors.id === "main--canvas") {
+      let target = event.target;
+      target.style.backgroundColor = `${selectedColor}`;
+    }
   };
   colors.onmouseout = function (event) {
-    let target = event.target;
-    target.style.backgroundColor = "blue";
+    if (colors.id === "main--canvas") {
+      let target = event.target;
+      target.style.backgroundColor = `${selectedColor}`;
+    }
   };
 }
+
 //Adds the class of color to the created divs
 function addColorClass() {
   let children = document
