fbf26edfc73504b3949bebf81a7751371bdfb474
Deerdevil <nicholai.oj@gmail.com>
2022-04-09 17:33:36 +0200

    Finished the app logic, adding more features later. CSS next.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict:";
+//Selectors
 const canvas = document.querySelector(".canvas");
 const mainCanvas = document.getElementById("start--canvas");
 const canvasButtons = document.querySelector(".canvas--buttons");
@@ -10,6 +11,10 @@ const toolPickColor = document.querySelector(".tool--one");
 const toolRandomColor = document.querySelector(".tool--two");
 const toolEraser = document.querySelector(".tool--three");
 const openColorPicker = document.getElementById("color--picker");
+//Booleans
+//Sets if the color should be random
+let randomChoice = false;
+
 //Buttons and clicks
 
 btnCanvas16.addEventListener("click", createCanvas16);
@@ -17,43 +22,35 @@ btnCanvas32.addEventListener("click", createCanvas32);
 btnCanvas64.addEventListener("click", createCanvas64);
 btnReset.addEventListener("click", resetCanvas);
 toolPickColor.addEventListener("click", colorPicker);
-// toolRandomColor.addEventListener("click", pickRandomColor);
-// toolEraser.addEventListener("click", eraser);
 
-function createElements(SelectClass, CreateElement, iterations) {
-  for (let i = 0; i < iterations; i++) {
-    let classBe = document.querySelector(SelectClass);
-    let elementBe = document.createElement(CreateElement);
-    classBe.appendChild(elementBe);
-  }
-  console.log(iterations);
-}
+toolEraser.addEventListener("click", function () {
+  randomChoice = false;
+  toolChoice = "white";
+});
 
-let pickedTool = false;
 function colorPicker() {
+  randomChoice = false;
+  openColorPicker.replaceChildren();
   openColorPicker.classList.remove("hidden");
   openColorPicker.classList.add("active");
-  pickedTool = true;
   createElements(".color--picker", "div", 8);
-  console.log(createElements);
-
   addClassToChildren("color--picker", "div", "color--picker--colors");
   addRandomColor("color--picker", "div");
-
   const colorpick = document.getElementById("color--picker");
-
+  //Choose color and close colorpicker
   colorpick.addEventListener("click", function (event) {
     let target = event.target;
     toolChoice = target.style.backgroundColor;
-    console.log(toolChoice);
+    openColorPicker.classList.add("hidden");
+    openColorPicker.classList.remove("active");
   });
 }
 
-// function toolChoice() {
-//   if(pickedTool && )
-// }
+toolRandomColor.addEventListener("click", function () {
+  mainCanvas.id = "main--canvas";
+  randomChoice = true;
+});
 
-// randomColor(randomColorArr)
 //Create canvas
 function createCanvas16() {
   mainCanvas.classList.remove("hidden");
@@ -90,20 +87,27 @@ function resetCanvas() {
   document.querySelector(".canvas").style.backgroundColor = "";
   mainCanvas.style.backgroundColor = "";
   mainCanvas.id = "start--canvas";
+  randomChoice = false;
+}
+//Create DIVs to populate the grid
+function createElements(SelectClass, CreateElement, iterations) {
+  for (let i = 0; i < iterations; i++) {
+    let classBe = document.querySelector(SelectClass);
+    let elementBe = document.createElement(CreateElement);
+    classBe.appendChild(elementBe);
+  }
 }
 //Adds class to the created divs
 function addClassToChildren(id, tag, className) {
   let children = document.getElementById(id).getElementsByTagName(tag);
-
   for (let i = 0; i < children.length; i++) {
     children[i].classList.add(className);
   }
 }
-//Color picker add colors
+//Add random colors to the color picker
 
 function addRandomColor(id, tag) {
   let children = document.getElementById(id).getElementsByTagName(tag);
-
   for (let i = 0; i < children.length; i++) {
     children[i].style.backgroundColor = `${randomColorArr[i]}`;
   }
@@ -121,6 +125,7 @@ let randomColorArr = [
   "yellow",
   "orange",
 ];
+
 function randomColor(arr) {
   const randomIndex = Math.floor(Math.random() * arr.length);
   const item = arr[randomIndex];
@@ -139,21 +144,25 @@ function getCursorPos(mainCanvas) {
     holding = false;
   };
   colors.onmousemove = function (event) {
-    if (holding === true) {
+    if (holding === true && randomChoice === false) {
       let target = event.target;
       target.style.backgroundColor = `${toolChoice}`;
     }
   };
 }
-mainCanvas.addEventListener("mousedown", function (e) {
+mainCanvas.addEventListener("mousedown", function () {
   holding = true;
 });
-mainCanvas.addEventListener("mouseup", function (e) {
+mainCanvas.addEventListener("mouseup", function () {
   holding = false;
 });
 mainCanvas.addEventListener("mousemove", function (e) {
   if (holding === true) {
     // draw();
     getCursorPos(mainCanvas, e);
+    if (randomChoice === true) {
+      let target = e.target;
+      target.style.backgroundColor = `${randomColor(randomColorArr)}`;
+    }
   }
 });
